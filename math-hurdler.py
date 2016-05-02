d4d3a7d351e16b874b287a7f1ae6a9b5fb58efe5
Ben Kantor <bkantor01@gmail.com>
2016-05-02 16:17:49 -0400

    Make horse jump hurdles and trot

--- math-hurdler.py
+++ math-hurdler.py
@@ -17,9 +17,12 @@ class MathHurdler:
         self.vy = 0
 
         self.paused = False
-        self.direction = 1
+        self.direction = -1
 
         self.circle_size = 150
+
+        self.horse_change_semaphore = 3;
+        self.horse_change = 0;
 
     def set_paused(self, paused):
         self.paused = paused
@@ -39,6 +42,7 @@ class MathHurdler:
     def run(self):
         self.running = True
 
+        display_info = pygame.display.Info();
         background_color = (126, 192, 238)
 
         screen = pygame.display.get_surface()
@@ -55,11 +59,17 @@ class MathHurdler:
 
         horse = pygame.image.load(self.get_asset_path('color_unicorn.png'))
         horse = pygame.transform.scale(horse,(horse.get_width() / 3, horse.get_height() / 3))
+        horse_jump = pygame.transform.rotate(horse,45)
+        horse_gallop = pygame.transform.rotate(horse, -15)
+
+        active_horse = horse_gallop;
+
+        horse_x = display_info.current_h/3;
 
         hurdle = pygame.image.load('./assets/images/hurdle.png')
         hurdle = pygame.transform.scale(hurdle,(hurdle.get_height()/3,hurdle.get_width()/3))
 
-        display_info = pygame.display.Info();
+        hurdle_y = display_info.current_h - hurdle.get_height() - (ground.get_height() / 2)
 
         while self.running:
             while Gtk.events_pending():
@@ -83,20 +93,33 @@ class MathHurdler:
                 elif self.direction == -1 and self.x < -50:
                     self.x = screen.get_width() + 50
 
-                self.y += self.vy
-                if self.y > screen.get_height() - 50:
-                    self.y = screen.get_height() - 50
-                    self.vy = -self.vy
+                if (self.horse_change == self.horse_change_semaphore):
+                    if (active_horse == horse):
+                        active_horse = horse_gallop
+                    elif (active_horse == horse_gallop):
+                        active_horse = horse
+                    elif (active_horse == horse_jump):
+                        active_horse = horse
+                    self.horse_change = 0
 
-                self.vy += 5
+                self.horse_change += 1
+
+                self.y = display_info.current_h - horse.get_height() - ground.get_height();
+
+                # Check if hurdle and horse in same spot.
+                hurdle_rect = hurdle.get_rect(topleft=(self.x,hurdle_y))
+                horse_rect = horse.get_rect(topleft=(horse_x,self.y))
+                if hurdle_rect.colliderect(horse_rect):
+                    active_horse = horse_jump
+                    self.y -= 200
 
             # Set the "sky" color to blue
             screen.fill(background_color)
 
             screen.blit(sun, (screen_size[1] + sun.get_width(), 0))
             screen.blit(ground, (0, screen_size[1] - ground.get_height()))
-            screen.blit(horse, (self.x, (display_info.current_h - horse.get_height() - ground.get_height())))
-            screen.blit(hurdle,(self.x,(display_info.current_h - hurdle.get_height()-ground.get_height())))
+            screen.blit(active_horse, (horse_x, self.y))
+            screen.blit(hurdle,(self.x,hurdle_y))
 
             # Draw the frame
             pygame.display.flip()
