c159ba40c29439ebd7bd8782a9e825fe31088d54
James Arnold <jra2196@rit.edu>
2016-05-04 12:19:00 -0400

    Started adding menu and answer checking. Also commented out Gtk for now because Windows doesn't like it.

--- math-hurdler.py
+++ math-hurdler.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python2
 
-import gi
+#import gi
 
-gi.require_version('Gtk', '3.0')
+#gi.require_version('Gtk', '3.0')
 
-import gi.repository.Gtk
+#import gi.repository.Gtk
 import pygame
 import random
 import os
@@ -47,6 +47,14 @@ class MathHurdler:
     def set_paused(self, paused):
         self.paused = paused
 
+    def set_gameover(self, gameover):
+        self.gameover = gameover
+
+    def set_playing(self, playing):
+        self.playing = playing
+        self.set_paused(False)
+        self.set_gameover(False)
+
     # Called to save the state of the game to the Journal.
     def write_file(self, file_path):
         pass
@@ -61,6 +69,11 @@ class MathHurdler:
     # The main game loop.
     def run(self):
         self.running = True
+        self.playing = False
+        self.gameover = False
+        last_answer = -1
+        question_dirty = True
+        
 
         display_info = pygame.display.Info()
         background_color = (126, 192, 238)
@@ -94,6 +107,7 @@ class MathHurdler:
         horse = pygame.transform.scale(horse,(horse.get_width() / 3, horse.get_height() / 3))
         horse_jump = pygame.transform.rotate(horse,45)
         horse_gallop = pygame.transform.rotate(horse, -15)
+        horse_dead = pygame.transform.rotate(horse, -180)
 
         active_horse = horse_gallop
 
@@ -107,6 +121,11 @@ class MathHurdler:
         question_board = pygame.Surface((screen_size[0]/3, screen_size[1] / 5))
         question_board = question_board.convert()
         question_board.fill((255, 255, 255))
+
+        menu_button = Button( 'Play', self.lg_font,(0,0,0), 200,
+                           100, (255,255,255), (0,0,0), -2)
+
+        pause_label = self.lg_font.render('PAUSED',1,(0,0,0))
 
         def answer_problem():
             self.question = Question()
@@ -122,101 +141,151 @@ class MathHurdler:
             self.points += 100
             self.score_label = self.lg_font.render(str(self.points),1,(0,0,0))
 
+        def set_answer(answer):
+            last_answer = answer
+
+        def evaluate_answer(answer):
+            if self.question.answer == answer:
+                self.set_gameover(True)
 
         while self.running:
-            while gi.repository.Gtk.events_pending():
-                gi.repository.Gtk.main_iteration()
+            #while gi.repository.Gtk.events_pending():
+            #    gi.repository.Gtk.main_iteration()
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    return
-                elif event.type == pygame.VIDEORESIZE:
-                    pygame.display.set_mode(event.size, pygame.RESIZABLE)
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        self.direction = 1
-                    elif event.key == pygame.K_p:
-                        self.paused = not self.paused
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    button_a.mouse_click(pygame.mouse.get_pos(),answer_problem)
+            if self.playing:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        return
+                    elif event.type == pygame.VIDEORESIZE:
+                        pygame.display.set_mode(event.size, pygame.RESIZABLE)
+                    elif event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_RIGHT:
+                            self.direction = 1
+                        elif event.key == pygame.K_p:
+                            self.paused = not self.paused
+                    elif event.type == pygame.MOUSEBUTTONDOWN:
+                        button_a.mouse_click(pygame.mouse.get_pos(),answer_problem)
 
-            screen_size = screen.get_size()
+                screen_size = screen.get_size()
 
-            if not self.paused:
 
-                self.x += self.vx * self.direction
-                if self.direction == 1 and self.x > screen.get_width() + 50:
-                    self.x = -50
-                elif self.direction == -1 and self.x < -50:
-                    self.x = screen.get_width() + 50
-                self.y = display_info.current_h - horse.get_height() - ground.get_height()
+                if not self.paused and not self.gameover:
 
-                hurdle_rect = hurdle.get_rect(topleft=(self.x,hurdle_y))
-                horse_rect = horse.get_rect(topleft=(horse_x,self.y))
+                    self.x += self.vx * self.direction
+                    if self.direction == 1 and self.x > screen.get_width() + 50:
+                        self.x = -50
+                    elif self.direction == -1 and self.x < -50:
+                        self.x = screen.get_width() + 50
+                    self.y = display_info.current_h - horse.get_height() - ground.get_height()
 
-                if (active_horse == horse_jump) and (not hurdle_rect.colliderect(horse_rect)):
-                    active_horse = horse
+                    hurdle_rect = hurdle.get_rect(topleft=(self.x,hurdle_y))
+                    horse_rect = horse.get_rect(topleft=(horse_x,self.y))
 
-                if (self.horse_change == self.horse_change_semaphore):
-                    if (active_horse == horse):
-                        active_horse = horse_gallop
-                    elif (active_horse == horse_gallop):
+                    if (active_horse == horse_jump) and (not hurdle_rect.colliderect(horse_rect)):
                         active_horse = horse
-                    self.horse_change = 0
 
-                self.horse_change += 1
+                    if (self.horse_change == self.horse_change_semaphore):
+                        if (active_horse == horse):
+                            active_horse = horse_gallop
+                        elif (active_horse == horse_gallop):
+                            active_horse = horse
+                        self.horse_change = 0
 
-                # Check if hurdle and horse in same spot.
-                if hurdle_rect.colliderect(horse_rect):
-                    active_horse = horse_jump
-                    self.y -= 200
+                    self.horse_change += 1
 
-            # Set the "sky" color to blue
-            screen.fill(background_color)
+                    # Check if hurdle and horse in same spot.
+                    if hurdle_rect.colliderect(horse_rect):
+                        #evaluate answer on first frame of hurdle collision
+                        if not question_dirty:
+                            evaluate_answer(last_answer)
+                            question_dirty = True
 
-            sun = Sun()
-            sun.rect.x = screen_size[1] + sun.image.get_width()
-            sun.rect.y = 0
+                        #if not gameover, jump the hurdle
+                        if not self.gameover:
+                            active_horse = horse_jump
+                            self.y -= 200
 
-            allsprites = pygame.sprite.RenderPlain((sun))
-            allsprites.draw(screen)
+                    #if not colliding with hurdle and question still dirty, generate new question
+                    else if question_dirty:
+                        answer_problem()
+                        question_dirty = False
+                        last_answer = -1
 
-            screen.blit(question_board, (screen_size[0] / 4, screen_size[1] / 5))
-            question_board.blit(self.question_label, (10,10))
-            question_board.blit(self.question_text_label, (10,self.question_label.get_height()+10))
+                if self.gameover:
+                    #spin the horse
+                    active_horse = pygame.transform.rotate(horse, pygame.time.get_ticks())
 
-            screen.blit(self.score_label, (sun.rect.x+sun.image.get_width()/4,sun.rect.y+sun.image.get_height()/3))
-            screen.blit(points_label, (sun.rect.x+sun.image.get_width()/4, sun.rect.y+sun.image.get_height()/3+self.score_label.get_height()))
+                # Set the "sky" color to blue
+                screen.fill(background_color)
 
-            screen.blit(ground, (0, screen_size[1] - ground.get_height()))
-            button_panel_x = ground.get_width()/4
-            button_panel_y = screen_size[1] - ground.get_height() + ground.get_height()/3+10
-            screen.blit(button_panel, (button_panel_x,button_panel_y))
+                sun = Sun()
+                sun.rect.x = screen_size[1] + sun.image.get_width()
+                sun.rect.y = 0
 
-            button_a.rect.x = button_panel_x
-            button_a.rect.y = button_panel_y
-            button_a.draw(screen)
+                allsprites = pygame.sprite.RenderPlain((sun))
+                allsprites.draw(screen)
 
-            button_b.rect.x = button_panel_x + button_a.image.get_width()
-            button_b.rect.y = button_panel_y
-            button_b.draw(screen)
+                screen.blit(question_board, (screen_size[0] / 4, screen_size[1] / 5))
+                question_board.blit(self.question_label, (10,10))
+                question_board.blit(self.question_text_label, (10,self.question_label.get_height()+10))
 
-            button_c.rect.x = button_panel_x
-            button_c.rect.y = button_panel_y + button_a.image.get_height()
-            button_c.draw(screen)
+                screen.blit(self.score_label, (sun.rect.x+sun.image.get_width()/4,sun.rect.y+sun.image.get_height()/3))
+                screen.blit(points_label, (sun.rect.x+sun.image.get_width()/4, sun.rect.y+sun.image.get_height()/3+self.score_label.get_height()))
 
-            button_d.rect.x = button_panel_x + button_c.image.get_width()
-            button_d.rect.y = button_panel_y + button_b.image.get_height()
-            button_d.draw(screen)
+                screen.blit(ground, (0, screen_size[1] - ground.get_height()))
+                button_panel_x = ground.get_width()/4
+                button_panel_y = screen_size[1] - ground.get_height() + ground.get_height()/3+10
+                screen.blit(button_panel, (button_panel_x,button_panel_y))
 
-            screen.blit(active_horse, (horse_x, self.y))
-            screen.blit(hurdle,(self.x,hurdle_y))
+                button_a.rect.x = button_panel_x
+                button_a.rect.y = button_panel_y
+                button_a.draw(screen)
 
-            # Draw the frame
-            pygame.display.flip()
+                button_b.rect.x = button_panel_x + button_a.image.get_width()
+                button_b.rect.y = button_panel_y
+                button_b.draw(screen)
 
-            # Try to stay at 30 FPS
-            self.clock.tick(30)
+                button_c.rect.x = button_panel_x
+                button_c.rect.y = button_panel_y + button_a.image.get_height()
+                button_c.draw(screen)
+
+                button_d.rect.x = button_panel_x + button_c.image.get_width()
+                button_d.rect.y = button_panel_y + button_b.image.get_height()
+                button_d.draw(screen)
+
+                screen.blit(active_horse, (horse_x, self.y))
+                screen.blit(hurdle,(self.x,hurdle_y))
+
+                # Draw the frame
+                pygame.display.flip()
+
+                # Try to stay at 30 FPS
+                self.clock.tick(30)
+                
+            else:
+                #in the menu
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        return
+                    elif event.type == pygame.VIDEORESIZE:
+                        pygame.display.set_mode(event.size, pygame.RESIZABLE)
+                    elif event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_p:
+                            self.set_playing(True)
+
+                screen_size = screen.get_size()
+                
+                screen.fill(background_color);
+                
+                menu_button.rect.x = (screen_size[0] - menu_button.rect.width) / 2
+                menu_button.rect.y = (screen_size[1] - menu_button.rect.height) / 2
+                menu_button.draw(screen)
+                
+                pygame.display.flip()
+                
+                # Try to stay at 30 FPS
+                self.clock.tick(30)
+
 
 # This function is called when the game is run directly from the command line:
 # ./TestGame.py
